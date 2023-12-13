baf9229fb43cb29440bd27cd3b229ee8f1f8f5d7
ashleymuka <153102801+ashleymuka@users.noreply.github.com>
2023-12-12 22:03:30 -0600

    Update deanslist.py

--- deanslist.py
+++ deanslist.py
@@ -1,12 +1,6 @@
 '''
-Author:Ashley Muka
-Assignment Title: Dean's List
-Assignment Description: Implement class Course to return a list of students that made the dean's list
-Due Date: 09/01/2023
-Date Created:08/29/2023
-Date Last Modified:08/31/2023
+Description: Implement class Course to return a list of students that made the dean's list
 '''
-
 
 class Student:
     def __init__(self, first, last, gpa):
@@ -22,7 +16,6 @@ class Student:
 
     def to_string(self):
         return self.first + ' ' + self.last + ' (GPA: ' + str(self.gpa) + ')'
-
 
 class Course:
     def __init__(self):
@@ -40,22 +33,15 @@ class Course:
 
 
 if __name__ == "__main__":
-    #Data abstraction
     
     course = Course()
-    
-    #input
-    
+        
     course.add_student(Student('Henry','Nguyen',3.5))
     course.add_student(Student('Brenda','Stern',2.0))
     course.add_student(Student('Lynda','Robinson',3.2))
     course.add_student(Student('Sonya','King',3.9))
-
-    #process
     
     deansList = course.get_deans_list()
-
-    #output
 
     print("Dean's list:")
     for student in deansList:
